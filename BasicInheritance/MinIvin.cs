8b5519759e635cb879d00af6684ba36d386f19f6
wuqi <1379121780@qq.com>
2023-11-19 11:28:53 +0800

    完成第5章第一次学习；开始第六章前部分第一次学习。

---
+++ BasicInheritance/MinIvin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    internal class MinIvin:Car
+    {
+
+        public void TestMethod()
+        {
+            //可以访问父类中的公共成员
+            Speed = 10;
+            //不可以访问父类中的私有成员
+            //currSpeed = 10;
+        }
+    }
+}
---
+++ BasicInheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMessage("====Basic Inheritance====");
+            Car myCar = new Car(80);
+            myCar.Speed = 50;
+            Console.WriteLine("My car is goint {0} MPH", myCar.Speed);
+
+            PrintMessage("==== extends fater====");
+            MinIvin min = new MinIvin();
+            //并没有继承 父类的构造方法，下方语句编译会报错
+            //MinIvin minIvin = new MinIvin(100);
+            min.Speed = 40;
+            Console.WriteLine("My min is going {0} MPH", min.Speed);
+            //min.currSpeed = 10;
+
+        }
+        static void PrintMessage(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
